93642264a49ad950d0e16f15fb39c3f32a3dda2d
parthbhangla <parthbhangla987@gmail.com>
2024-01-23 19:34:00 -0500

    done for the day

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class WordlePlayer:
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("--disable-notifications")
         self.driver = webdriver.Chrome(options = self.options)
-        self.word_l = self.load()
+        self.word_list = self.load()
 
     def load(self):
         word_list = []
@@ -38,10 +38,32 @@ class WordlePlayer:
             button.click()
         except Exception as e:
             print(e)
-        time.sleep(2)
+        time.sleep(1)
 
         # wordle inspect: there's three states - absent, present and correct so modify list on that basis
+        # it also allows only 6 attemps, so could be a loop idea
+        # random word from the file to start and over again to second guess
 
+        # basic idea:
+        # guess a word, start a loop (maybe on attempt basis), check states of the letters in the guess
+        # on the basis on states of the letters, search and eliminate words from the original list
+        # pick another random word and run the same loop over and over again until correct word = Found
+
+        self.load() # loading the list
+
+        word = random.choice(self.word_list) # random word from the list
+        print(word) # check
+
+        attempts = 0 # setting attempts to 0
+        guess = False # variable for future check
+
+        for attempts in range (0, 6):
+            attempts = attempts + 1
+            word = random.choice(self.word_list)
+            letters = []
+            for letter in word:
+                letters.append(letter)
+                button = self.driver.find_element(by = "class name", value = "")
 
 if __name__ == "__main__":
     WordlePlayer().play()
